65d02a9fb13148add1607cb41592541c165cd4cb
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-04-24 18:29:26 -0300

    menu.blade.php added

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('content')
+    <h1>{{ $title }}</h1>
+    <hr>
+    <!-- en remplazo de @if (!empty($users)) @endif tambien puedes usar 
+    @unless (empty($users)) @endunless OR @empty($users) cambiar orden aqui @endempty -->
+    @if (!empty($users)) 
+        <ul>
+            @foreach($users as $user)
+                <li>{{ $user }}</li>
+            @endforeach
+        </ul>
+            @else
+                <p>No hay usuarios registrados.</p>
+            @endif
+            <!-- Tambien puedes reemplazar el bloque de codigo anterior por esto
+                <ul>
+                    @forelse ($users as $user)
+                        <li>{{ $user }}</li>
+                    @empty
+                        <li>No hay usuarios registrados.</li>
+                    @endforelse
+                </ul>
+            -->
+@endsection
+
+@section('sidebar')
+    @parent
+    <h2>Barra lateral personalizada</h2>
+@endsection
