3f3652a6dca3234b26389e9cd9b28d19d5e4432b
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-04-24 13:01:21 -0300

    Vistas en laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,37 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return 'Usuarios';
+
+        $users = [
+            'Gerardo',
+            'Mario',
+            'Alvaro',
+            '<script>alert("Clicker")</script>'
+        ];
+
+        $title = "Listado de usuarios";
+
+        /*return view('users')->with([
+            'users' => $users,
+            'title' => 'Listado de usuarios'
+        ]);
+
+        OR
+
+        return view('users')
+            ->with('users', $users)
+            ->with('title', 'Listado de usuarios');
+        
+        OR
+
+        return view('users', [
+            'users' => $users,
+            'title' => $title
+        ]);*/
+
+        //dd(compact('title', 'users')); //dd esquivale a hacer un var_dump() & die; en php
+
+        return view('users', compact('title', 'users'));
     }
 
     public function show($id){
--- resources/views/users.php
+++ resources/views/users.php
@@ -9,6 +9,12 @@
     <script src="main.js"></script>
 </head>
 <body>
-    <h1>Usuarios</h1>
+    <h1><?= e($title) ?></h1>
+
+    <ul>
+        <?php foreach($users as $user): ?>
+            <li><?= e($user) ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
