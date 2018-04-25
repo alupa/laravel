a9cd1415dc4b39220d7ac73a0e68ee44823b5acf
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-04-25 16:46:37 -0300

    Inserción de datos con los seeders de Laravel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTables([
+            'users',
+            'professions'
+        ]);
         // $this->call(UsersTableSeeder::class);
+        $this->call(ProfessionSeeder::class);
+        $this->call(UserSeeder::class);
+    }
+
+    protected function truncateTables(array $tables){
+        // Sentencia SQL para que no chequee foreign keys en la tabla
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        
+        foreach($tables as $table){
+            // Elimina toda la data de la tabla
+            DB::table($table)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
