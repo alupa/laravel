3f17b76b24f9d88bf3c3efdbb00db10e6e0b7703
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-04-30 15:33:08 -0300

    Creación de formulario para agregar usuarios

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,14 +1,25 @@
 @extends('layout')
 
-@section('title', "UCrear usuario")
+@section('title', "Crear usuario")
 
 @section('content')
     <h1>Crear usuario</h1>
     
     <form method="POST" action="{{ url('usuarios') }}">
         {{ csrf_field() }}
-        <button type="submit">Crear usuario</button>
+        <div class="form-group">
+            <label for="name">Nombre:</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Jhon Doe">
+        </div>
+        <div class="form-group">
+            <label for="email">Correo electrónico:</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="jhon@doe.com">
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Mayor a 6 caracteres">
+        </div>
+        <button type="submit" class="btn btn-primary">Crear usuario</button>
+        <a class="btn btn-link" href="{{ route('users.index')}}">Regresar</a>
     </form>
-
-    <p><a href="{{ route('users.index')}}">Regresar</a></p>
 @endsection
