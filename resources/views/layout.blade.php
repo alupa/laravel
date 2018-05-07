1c7786c8edc48b60bcb8da4a91efaa1c74bcbb06
Alvaro Lupa <alvaro.lupa@gmail.com>
2018-05-07 12:08:03 -0300

    Agregar estilo al listado de usuarios

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/style.css')}}" rel="stylesheet">
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,45 +1,49 @@
 @extends('layout')
 
 @section('content')
-    <h1>{{ $title }}</h1>
-    <a href="{{ route('users.create') }}">Nuevo usuario</a>
-    <hr>
+    <div class="d-flex justify-content-between align-items-end mb-2">
+        <h1 class="pb-1">{{ $title }}</h1>
+        <a href="{{ route('users.create') }}" class="btn btn-primary">Nuevo usuario</a>
+    </div>
+    
+    {{-- en remplazo de @if (!empty($users)) @endif tambien puedes usar 
+    @unless (empty($users)) @endunless OR @empty($users) cambiar orden aqui @endempty --}}
 
-    <!-- en remplazo de @if (!empty($users)) @endif tambien puedes usar 
-    @unless (empty($users)) @endunless OR @empty($users) cambiar orden aqui @endempty -->
-
-    <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Correo electrónico</th>
-                <th scope="col">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse ($users as $user)
+    @if ($users->isNotEmpty())
+        <table class="table table-striped">
+            <thead class="thead-dark">
                 <tr>
-                    <th scope="row">{{ $user->id }}</th>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td><a href="{{ route('users.show', $user) }}">Ver detalles</a> |
-                    <!-- OR url("usuarios/{$user->id}") OR action('UserController@show', ['id' => $user->id]) -->
-                        <a href="{{ route('users.edit', $user) }}">Editar</a>
-                    <form action="{{ route('users.destroy', $user) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Eliminar</button>
-                    </form>
-                    </td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Correo electrónico</th>
+                    <th scope="col">Acciones</th>
                 </tr>
-            @empty
-                <p>No hay usuarios registrados.</p>
-            @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                        <form action="{{ route('users.destroy', $user) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <a href="{{ route('users.show', $user) }}" class="btn btn-link"><i class="fas fa-eye"></i></a>
+                            <!-- OR url("usuarios/{$user->id}") OR action('UserController@show', ['id' => $user->id]) -->
+                            <a href="{{ route('users.edit', $user) }}" class="btn btn-link"><i class="far fa-edit"></i></a>
+                            <button type="submit" class="btn btn-link"><i class="fas fa-trash"></i></button>
+                        </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No hay usuarios registrados.</p>
+    @endif
 
-    <!--
+    {{--
         @if (!empty($users)) 
         <ul>
         @foreach($users as $user)
@@ -49,9 +53,9 @@
         @else
             <p>No hay usuarios registrados.</p>
         @endif
-    -->
+    --}}
 
-            <!-- Tambien puedes reemplazar el bloque de codigo anterior por esto
+            {{-- Tambien puedes reemplazar el bloque de codigo anterior por esto
                 <ul>
                     @forelse ($users as $user)
                         <li>{{ $user->name }}</li>
@@ -59,7 +63,7 @@
                         <li>No hay usuarios registrados.</li>
                     @endforelse
                 </ul>
-            -->
+            --}}
 @endsection
 
 @section('sidebar')
